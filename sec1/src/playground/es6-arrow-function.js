0fa4277cc488af6cb45f43b7a57a37cf4271b520
daas-ankur-shukla <work.ankurshukla@gmail.com>
2018-04-04 02:49:17 +0530

    Challenge 3

---
+++ sec1/src/playground/es6-arrow-function.js
@@ -0,0 +1,8 @@
+const getFirstName = (fullName) => {
+  return fullName.split(' ')[0];
+};
+
+const getFirstNameS = (fullName) => fullName.split(' ')[0];
+
+console.log(getFirstName('Ankur Shukla'));
+console.log(getFirstNameS('Ankur Shukla'));
