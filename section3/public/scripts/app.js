3be01078850635f529586af98355910f6a0d33c3
daas-ankur-shukla <work.ankurshukla@gmail.com>
2018-10-04 19:53:00 +0530

    Buildit Visibility Lecture 20

--- section3/public/scripts/app.js
+++ section3/public/scripts/app.js
@@ -1,77 +1,38 @@
 'use strict';
 
-console.log('App.js is running');
+var visibility = false;
 
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in hands of a computer',
-  options: []
+var toggleVisibility = function toggleVisibility() {
+  visibility = !visibility;
+  render();
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-  }
-  renderApp();
-};
-
-var resetOptions = function resetOptions() {
-  app.options = [];
-  renderApp();
-};
-
-var renderApp = function renderApp() {
-  var template = React.createElement(
+var render = function render() {
+  var jsx = React.createElement(
     'div',
     null,
     React.createElement(
       'h1',
       null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      'p',
-      null,
-      app.subtitile
+      'Visibility Toggle'
     ),
     React.createElement(
-      'p',
-      null,
-      app.options.length > 0 ? 'Here are your ' + app.options.length + ' options' : 'No options'
+      'button',
+      { onClick: toggleVisibility },
+      visibility ? 'Hide Details' : 'Show Details'
     ),
-    React.createElement(
-      'ol',
+    visibility && React.createElement(
+      'div',
       null,
-      app.options.length > 0 ? app.options.map(function (opt) {
-        return React.createElement(
-          'li',
-          { key: opt.toString() },
-          opt
-        );
-      }) : ''
-    ),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'option' }),
       React.createElement(
-        'button',
+        'p',
         null,
-        'Add Option'
-      ),
-      React.createElement(
-        'button',
-        { onClick: resetOptions },
-        'Reset'
+        'Hey. These are some details you can see'
       )
     )
   );
-  var appRoot = document.getElementById('app');
-  ReactDOM.render(template, appRoot);
+
+  ReactDOM.render(jsx, document.getElementById('app'));
 };
 
-renderApp();
+render();
