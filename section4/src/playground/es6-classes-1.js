a8f6d08d6d4631aeab605f147c62435e2af4b91e
daas-ankur-shukla <work.ankurshukla@gmail.com>
2018-12-24 17:26:33 +0530

    section 4 challenge 9 lecture 25

--- section4/src/playground/es6-classes-1.js
+++ section4/src/playground/es6-classes-1.js
@@ -4,16 +4,35 @@ class Person {
     this.age = age;
   }
   getGreeting() {
-    return `Hi. I am ${ this.name }`;
+    return `Hi. I am ${ this.name }. `;
   }
   getDescription() {
-    return `${ this.name } is ${ this.age } year(s) old.`;
+    return `${ this.name } is ${ this.age } year(s) old. `;
   }
 }
 
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  hasHomeLocation() {
+    console.log(this.homeLocation)
+    return !!this.homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
 
-const me = new Person('Andrew Mead', 26);
-console.log(me.getDescription())
+    if(this.homeLocation) {
+      greeting += `I am visiting ${ this.homeLocation }`;
+    }
 
-const other = new Person();
-console.log(other.getDescription())
+    return greeting;
+  }
+}
+
+const me = new Traveler('Andrew Mead', 26, 'Philedelphia');
+console.log(me.getGreeting())
+
+const other = new Traveler();
+console.log(other.getGreeting())
